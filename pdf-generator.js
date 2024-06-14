71f15442e810a0e2af0927c846ff1933c64e3696
Amaan-Majid <amaan.majid@klipit.co>
2024-06-14 17:45:34 +0400

    pdf generation API

--- pdf-generator.js
+++ pdf-generator.js
@@ -1,55 +1,115 @@
 const fs = require("fs");
 const path = require("path");
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 const handlebars = require("handlebars");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.listen(4554, () => {
+  console.log("server listening on port 4554");
+});
+app.post("/pdf", async (req, res) => {
+  try {
+    const { data } = req.body;
+    var templateHtml = fs.readFileSync(
+      path.join(process.cwd(), "template.html"),
+      "utf8"
+    );
+    var template = handlebars.compile(templateHtml);
+    var html = template(data);
 
-async function createPDF(data){
+    var options = {
+      width: "400px",
+      height: "400px",
+      headerTemplate: "<p></p>",
+      footerTemplate: "<p></p>",
+      displayHeaderFooter: false,
+      margin: {
+        top: "10px",
+        bottom: "30px",
+      },
+      printBackground: true,
+      path: `${data.product}.pdf`,
+    };
 
-	var templateHtml = fs.readFileSync(path.join(process.cwd(), 'template.html'), 'utf8');
-	var template = handlebars.compile(templateHtml);
-	var html = template(data);
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      headless: true,
+    });
 
-	var milis = new Date();
-	milis = milis.getTime();
+    var page = await browser.newPage();
 
-	var pdfPath = path.join('pdf', `${data.name}-${milis}.pdf`);
+    await page.goto(`data:text/html;charset=UTF-8,${html}`, {
+      waitUntil: "networkidle0",
+    });
 
-	var options = {
-		width: '1230px',
-		headerTemplate: "<p></p>",
-		footerTemplate: "<p></p>",
-		displayHeaderFooter: false,
-		margin: {
-			top: "10px",
-			bottom: "30px"
-		},
-		printBackground: true,
-		path: pdfPath
-	}
+    await page.pdf(options);
+    await browser.close();
+    res.send("done");
+  } catch (error) {
+    console.log(error);
+  }
+});
+// async function createPDF(data) {
+//   var templateHtml = fs.readFileSync(
+//     path.join(process.cwd(), "template.html"),
+//     "utf8"
+//   );
+//   var template = handlebars.compile(templateHtml);
+//   var html = template(data);
 
-	const browser = await puppeteer.launch({
-		args: ['--no-sandbox'],
-		headless: true
-	});
+//   var milis = new Date().getTime();
 
-	var page = await browser.newPage();
-	
-	await page.goto(`data:text/html;charset=UTF-8,${html}`, {
-		waitUntil: 'networkidle0'
-	});
+//   var options = {
+//     width: "400px",
+//     height: "400px",
+//     headerTemplate: "<p></p>",
+//     footerTemplate: "<p></p>",
+//     displayHeaderFooter: false,
+//     margin: {
+//       top: "10px",
+//       bottom: "30px",
+//     },
+//     printBackground: true,
+//     path: `${data.product}.pdf`, // Use dynamic path for the generated PDF
+//   };
 
-	await page.pdf(options);
-	await browser.close();
-}
+//   const browser = await puppeteer.launch({
+//     args: ["--no-sandbox"],
+//     headless: true,
+//   });
 
-const data = {
-	title: "A new Brazilian School",
-	date: "05/12/2018",
-	name: "Rodolfo Luis Marcos",
-	age: 28,
-	birthdate: "12/07/1990",
-	course: "Computer Science",
-	obs: "Graduated in 2014 by Federal University of Lavras, work with Full-Stack development and E-commerce."
-}
+//   var page = await browser.newPage();
 
-createPDF(data);
+//   await page.goto(`data:text/html;charset=UTF-8,${html}`, {
+//     waitUntil: "networkidle0",
+//   });
+
+//   await page.pdf(options);
+//   await browser.close();
+// }
+
+// const data1 = {
+//   title: "TRUKLIP CERTIFICATE",
+//   product: "iPhone 15 Pro Max",
+//   date: "14/06/2024",
+//   name: "Rodolfo Luis",
+//   truklipid: "TRU28323213",
+// };
+
+// const data2 = {
+//   title: "TRUKLIP CERTIFICATE",
+//   product: "Samsung Galaxy S23 Ultra",
+//   date: "14/06/2024",
+//   name: "Jane Doe",
+//   truklipid: "TRU98765432",
+// };
+
+// async function generatePDFs() {
+//   await createPDF(data1);
+//   await createPDF(data2);
+// }
+
+// generatePDFs();
