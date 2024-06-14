b959de2dc32b21e398ee6242fe76cfaf0a4a94a2
Amaan-Majid <amaan.majid@klipit.co>
2024-06-14 17:56:06 +0400

    generate dynamic number of pdf's

--- pdf-generator.js
+++ pdf-generator.js
@@ -13,103 +13,46 @@ app.listen(4554, () => {
 app.post("/pdf", async (req, res) => {
   try {
     const { data } = req.body;
-    var templateHtml = fs.readFileSync(
-      path.join(process.cwd(), "template.html"),
-      "utf8"
-    );
-    var template = handlebars.compile(templateHtml);
-    var html = template(data);
+    console.log(data.length);
+    for (let i = 0; i < data.length; i++) {
+      var templateHtml = fs.readFileSync(
+        path.join(process.cwd(), "template.html"),
+        "utf8"
+      );
+      var template = handlebars.compile(templateHtml);
+      var html = template(data[i]);
 
-    var options = {
-      width: "400px",
-      height: "400px",
-      headerTemplate: "<p></p>",
-      footerTemplate: "<p></p>",
-      displayHeaderFooter: false,
-      margin: {
-        top: "10px",
-        bottom: "30px",
-      },
-      printBackground: true,
-      path: `${data.product}.pdf`,
-    };
+      var options = {
+        width: "400px",
+        height: "400px",
+        headerTemplate: "<p></p>",
+        footerTemplate: "<p></p>",
+        displayHeaderFooter: false,
+        margin: {
+          top: "10px",
+          bottom: "30px",
+        },
+        printBackground: true,
+        path: `${data[i].product}.pdf`,
+      };
 
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox"],
-      headless: true,
-    });
+      const browser = await puppeteer.launch({
+        args: ["--no-sandbox"],
+        headless: true,
+      });
 
-    var page = await browser.newPage();
+      var page = await browser.newPage();
 
-    await page.goto(`data:text/html;charset=UTF-8,${html}`, {
-      waitUntil: "networkidle0",
-    });
+      await page.goto(`data:text/html;charset=UTF-8,${html}`, {
+        waitUntil: "networkidle0",
+      });
 
-    await page.pdf(options);
-    await browser.close();
+      await page.pdf(options);
+      await browser.close();
+    }
+
     res.send("done");
   } catch (error) {
     console.log(error);
   }
 });
-// async function createPDF(data) {
-//   var templateHtml = fs.readFileSync(
-//     path.join(process.cwd(), "template.html"),
-//     "utf8"
-//   );
-//   var template = handlebars.compile(templateHtml);
-//   var html = template(data);
-
-//   var milis = new Date().getTime();
-
-//   var options = {
-//     width: "400px",
-//     height: "400px",
-//     headerTemplate: "<p></p>",
-//     footerTemplate: "<p></p>",
-//     displayHeaderFooter: false,
-//     margin: {
-//       top: "10px",
-//       bottom: "30px",
-//     },
-//     printBackground: true,
-//     path: `${data.product}.pdf`, // Use dynamic path for the generated PDF
-//   };
-
-//   const browser = await puppeteer.launch({
-//     args: ["--no-sandbox"],
-//     headless: true,
-//   });
-
-//   var page = await browser.newPage();
-
-//   await page.goto(`data:text/html;charset=UTF-8,${html}`, {
-//     waitUntil: "networkidle0",
-//   });
-
-//   await page.pdf(options);
-//   await browser.close();
-// }
-
-// const data1 = {
-//   title: "TRUKLIP CERTIFICATE",
-//   product: "iPhone 15 Pro Max",
-//   date: "14/06/2024",
-//   name: "Rodolfo Luis",
-//   truklipid: "TRU28323213",
-// };
-
-// const data2 = {
-//   title: "TRUKLIP CERTIFICATE",
-//   product: "Samsung Galaxy S23 Ultra",
-//   date: "14/06/2024",
-//   name: "Jane Doe",
-//   truklipid: "TRU98765432",
-// };
-
-// async function generatePDFs() {
-//   await createPDF(data1);
-//   await createPDF(data2);
-// }
-
-// generatePDFs();
